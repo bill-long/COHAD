af7b0018893e85afabf947a6cc9080b101e5aa33
Bill Long <bill@bill-long.com>
2019-05-03 22:56:17 -0500

    Remove route animations, too problematic

--- Web/ClientApp/src/app/app.component.ts
+++ Web/ClientApp/src/app/app.component.ts
@@ -1,17 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { fadeInAnimation } from './animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  animations: [fadeInAnimation]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-
-  prepareRoute(outlet: RouterOutlet) {
-    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
-  }
-  
-}
+export class AppComponent { }
