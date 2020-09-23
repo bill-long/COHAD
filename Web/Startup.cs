99c190cad7640b6d265207c195f4b73dc4c7897c
Bill Long <bilong@microsoft.com>
2020-09-23 01:16:54 -0500

    Add Application Insights

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -26,6 +26,8 @@ namespace Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplicationInsightsTelemetry();
+
             services.AddControllers();
 
             // In production, the Angular files will be served from this directory
