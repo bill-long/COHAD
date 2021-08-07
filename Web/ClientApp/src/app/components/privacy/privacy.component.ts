79b911775c9e95c2cf31745bdc1ae3b1a719cdba
Bill Long <bilong@microsoft.com>
2021-08-07 14:07:09 -0500

    Add privacy policy

---
+++ Web/ClientApp/src/app/components/privacy/privacy.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+    selector: 'app-privacy',
+    template: `
+        <div class="inner">
+            <h1>Privacy Policy</h1>
+            <div>
+                <ul>
+                    <li>
+                        When you register on this site, we collect your name and email address.
+                        We then associate that information with your street address in Canyon Oaks.
+                    </li>
+                    <li>
+                        Once we associate your account with your street address, your account is
+                        able to view the neighborhood directory. Only residents of
+                        Canyon Oaks may view this directory.
+                    </li>
+                    <li>
+                        You may configure your email addresses and phone numbers to be published
+                        in the directory on this web site, or not.
+                        If you choose to enter an email address or phone number which is not to
+                        be published in the directory, it is visible only to the administrators
+                        of the web site (currently Bill Long and Judy Johannesen).
+                    </li>
+                    <li>
+                        If you need help removing any of your information, just let us know.
+                    </li>
+                    <li>
+                        We do not sell any of your information, ever.
+                    </li>
+                </ul>
+            </div>
+        </div>
+  `,
+    styles: [`
+        .inner {
+            max-width: 600px;
+            margin: 0 auto;
+        }
+
+        li {
+            margin-bottom: 10px;
+        }
+    `]
+})
+export class PrivacyComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+}
