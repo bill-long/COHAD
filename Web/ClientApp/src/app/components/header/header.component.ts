5363fb5b8c526d29c73e3775c4488ad1cc59a48f
Bill Long <bilong@microsoft.com>
2026-03-20 01:08:04 -0500

    Image should fill viewport on Home

--- Web/ClientApp/src/app/components/header/header.component.ts
+++ Web/ClientApp/src/app/components/header/header.component.ts
@@ -2,10 +2,10 @@ import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
     selector: 'app-header',
-    inputs: ['title', 'subtitles', 'imagePath'],
+    inputs: ['title', 'subtitles', 'imagePath', 'noBottomGap', 'fillViewport'],
     styleUrls: ['./header.component.css'],
     template: `
-  <div class="header" [ngStyle]="{'background-image': 'url(' + imagePath + ')'}">
+  <div class="header" [class.no-bottom-gap]="noBottomGap" [class.fill-viewport]="fillViewport" [ngStyle]="{'background-image': 'url(' + imagePath + ')'}">
     <h1 class="mat-headline-1 font-weight-bold">{{title}}</h1>
     <h1 class="mat-headline-3" *ngFor="let subtitle of subtitles">{{subtitle}}</h1>
   </div>
@@ -17,6 +17,8 @@ export class HeaderComponent implements OnInit {
   title!: string;
   subtitles!: string[];
   imagePath!: string;
+  noBottomGap = false;
+  fillViewport = false;
 
   constructor() { }
 
